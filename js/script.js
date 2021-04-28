64ae207b396bd9aa9e7f858e2d137e9801498ea5
Patrick Coxall <mr.coxall@mths.ca>
2021-04-28 22:53:27 +0000

    fixed math

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 
 function doMathClicked () {
   document.getElementById('add-math').innerHTML = '6 + 5 = ' + (6 + 5)
-  document.getElementById('subtract-math').innerHTML = '7 - 3 =  ' + (7 + 3)
+  document.getElementById('subtract-math').innerHTML = '7 - 3 =  ' + (7 - 3)
   document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
   document.getElementById('divide-math').innerHTML = '(4 / 2) + 3 = ' + ((4 / 2) + 3)
   document.getElementById('exponent-math').innerHTML = '5 + 2 ** 3 = ' + (5 + 2 ** 3)
